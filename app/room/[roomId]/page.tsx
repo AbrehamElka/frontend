e6c540aba5f44b157caf6cc2c89302541e860cc3
AbrehamElka <abrehamdessalegn02@gmail.com>
2025-08-01 14:46:37 +0300

    user join room handling frontend

---
+++ app/room/[roomId]/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { useParams } from "next/navigation";
+import socket from "@/lib/socket";
+const Room = () => {
+  const { roomId } = useParams();
+  useEffect(() => {
+    socket.connect();
+    socket.on("connect", () => {
+      socket.emit("join-room", { roomId, userId: "123" });
+    });
+    socket.on("user-joined", (data) => {
+      console.log(data);
+    });
+  }, []);
+  return <div>Room {roomId}</div>;
+};
+
+export default Room;
